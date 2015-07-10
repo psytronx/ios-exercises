aab4844f92241f070a38b5664b1afa98a5c1bd01
Roger Hom <roger.c.hom@gmail.com>
2015-07-10 00:18:29 -0700

    Implement stringWithNumbersBetweenNumber:andOtherNumber: so that tests pass

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,26 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSMutableString *numbers = [NSMutableString stringWithString:@""];
+    NSString *numberAsString;
+    NSInteger i;
+    
+    if (otherNumber >= number){\
+        for (i = number; i <= otherNumber; i++){
+            numberAsString = [NSString stringWithFormat:@"%ld", (long)i];
+            [numbers appendString:numberAsString];
+        }
+    }
+    else {
+        for (i = otherNumber; i <= number; i++ ){
+            numberAsString = [NSString stringWithFormat:@"%ld", (long)i];
+            [numbers appendString:numberAsString];
+        }
+    }
+    
+    
+    return numbers;
 }
 
 @end
