798e2f55008af4be5a387d0b9124a093bdbe7220
Roger Hom <roger.c.hom@gmail.com>
2015-07-09 23:56:44 -0700

    Refactor dollarCostForAppleFlavoredVodka to use ternary operator. Implement itemMaryCanPurchaseForDollars: remaining tests pass.

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,6 +14,21 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
+    if (dollars <= 4){
+        itemToReturn = @"get out of my store";
+    }
+    else if (dollars == 5) {
+        itemToReturn = @"have some gum";
+    }
+    else if (dollars == 6) {
+        itemToReturn = @"have an apple";
+    }
+    else if (dollars <= 1000) {
+        itemToReturn = @"have an Apple computer";
+    }
+    else {
+        itemToReturn = @"have The Big Apple";
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
@@ -24,9 +39,7 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    cost = self.getsDiscount ? cost *= .75 : cost;
     
     return cost;
 }
