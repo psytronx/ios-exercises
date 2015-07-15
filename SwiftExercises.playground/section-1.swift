558487a75fc962c47f4ec31c9ad48e1db5540394
Roger Hom <roger.c.hom@gmail.com>
2015-07-15 00:42:09 -0700

    Implement swift exercises

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is \(cheese)."
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -20,13 +20,13 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
-// WORK HERE
+numberArray = numberArray + [5]
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
-// WORK HERE
+numberDictionary[5] = "five"
 
 /*
 
@@ -35,10 +35,14 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for i in 1...10 {
+    println(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for i in 1..<11 {
+    println(i)
+}
 
 let worf = [
     "name": "Worf",
@@ -58,7 +62,14 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var favDrinks:Array<String> = []
+    for character in characters {
+        if let favDrink = character["favorite drink"]{
+            favDrinks = favDrinks + [favDrink]
+        }
+    }
+    
+    return favDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -76,6 +87,15 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func mergeStrings(strings:Array<String>) -> String {
+    var mergedString:String = strings[0]
+    for stringItem in strings[1..<strings.count] {
+        mergedString = mergedString + ";" + stringItem
+    }
+    return mergedString
+}
+
+let output = mergeStrings(strings)
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -89,3 +109,10 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let sortedCerealArray = cerealArray.sorted({(item1:String, item2:String) -> Bool in
+    return item1 < item2
+})
+println(sortedCerealArray)
+
+
+
